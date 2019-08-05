e783a262ac76e99980aa1b02949e344e06ac59d9
EC2 Default User <ec2-user@ip-172-31-39-59.us-east-2.compute.internal>
2019-08-05 17:20:26 +0000

    No Stealing Accounts

--- app/routes.py
+++ app/routes.py
@@ -75,7 +75,9 @@ def index():
         
     else:
         error=None
-        
+        username = None
+        if session['username']:
+            username= session['username']
         return render_template('index.html',laptops=laptops,username=username,picture=picture)
 
 @app.route('/results',methods=['GET','POST'])
